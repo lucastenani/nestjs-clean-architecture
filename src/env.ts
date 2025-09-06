ca9b2facfa135d394929c66a4413bcfbc6dd1986
lucastenani <lucas.tenani3@gmail.com>
2025-09-05 22:59:24 -0300

    feat: add JWT and Passport modules for authentication

--- src/env.ts
+++ src/env.ts
@@ -6,6 +6,7 @@ export const envSchema = z.object({
 		.default('development'),
 	PORT: z.coerce.number().optional().default(3333),
 	DATABASE_URL: z.url(),
+	JWT_SECRET: z.string().min(1),
 })
 
 export type Env = z.infer<typeof envSchema>
