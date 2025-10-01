1928fe0df7e17f23d9bec96ef2868f0be1fd7de3
lucastenani <lucas.tenani3@gmail.com>
2025-10-01 09:33:14 -0300

    feat: remove userId column from questions table and restore initial migration structure

--- prisma/migrations/20251001023819_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `userId` on the `questions` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."questions" DROP COLUMN "userId";
