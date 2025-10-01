5d1549edff7341364b61a921f39b61cd8aeb05a4
lucastenani <lucas.tenani3@gmail.com>
2025-09-30 22:04:00 -0300

    feat: update JWT strategy and user decorator for improved type safety and payload validation

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,4 +1,6 @@
 import { Controller, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user-decorator'
+import type { UserPayload } from 'src/auth/jwt.strategy'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 
 @Controller('/questions')
@@ -7,7 +9,8 @@ export class CreateQuestionController {
 	constructor() {}
 
 	@Post()
-	async handle() {
+	handle(@CurrentUser() user: UserPayload) {
+		console.log(user)
 		return 'ok'
 	}
 }
